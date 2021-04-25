c1d713c69e4d04e3dc707bc02ac1e125964c04f3
Josh Yi <jyi468@gmail.com>
2021-04-25 15:48:57 -0400

    Add redirect when clicking outside of notification. Add stopPropagation to prevent when clicking inside modal box. Pass in props to modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
+
     return (
         <div>
-            <Modal/>
-        StreamDelete
+            StreamDelete
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     );
 }
