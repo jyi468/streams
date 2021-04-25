43e68d4d812731cb33642be93eac89c6cc4c697b
Josh Yi <jyi468@gmail.com>
2021-04-25 15:32:47 -0400

    Add modal using portals

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>;
+    return (
+        <div>
+            <Modal/>
+        StreamDelete
+        </div>
+    );
 }
 
 export default StreamDelete;
