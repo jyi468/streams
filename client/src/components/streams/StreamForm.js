e8be14029d0b59b6c6391e9d3b0a5dbd89a97f9a
jyi <joshua.yi@amadeus.com>
2021-05-07 13:03:51 -0400

    Refactor StreamForm into functional component.

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,43 +1,39 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
-class StreamForm extends React.Component {
-    renderError({error, touched}) {
-        if (touched && error) {
-            return (
-                <div className="ui error message">
-                    <div className="header">{error}</div>
-                </div>
-            );
-        }
-    }
-
-    renderInput = ({input, label, meta}) => {
-        // validate function passes error objects to render input's meta property
-        const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+// Move component declaration outside so that React does not render a new component every time
+const Error = ({error, touched}) => {
+    if (touched && error) {
         return (
-            <div className={className}>
-                <label>{label}</label>
-                <input {...input} autoComplete="off"/>
-                {this.renderError(meta)}
+            <div className="ui error message">
+                <div className="header">{error}</div>
             </div>
         );
-    };
-
-    onSubmit = (formValues) => {
-        this.props.onSubmit(formValues);
-    };
-
-    render() {
-        return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-                <Field name="title" component={this.renderInput} label="Enter Title"/>
-                <Field name="description" component={this.renderInput} label="Enter Description"/>
-                <button className="ui button primary">Submit</button>
-            </form>
-        );
     }
-}
+    return null;
+};
+
+const Input = ({input, label, meta}) => {
+    // validate function passes error objects to render input's meta property
+    const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+    return (
+        <div className={className}>
+            <label>{label}</label>
+            <input {...input} autoComplete="off"/>
+            <Error {...meta} />
+        </div>
+    );
+};
+
+const StreamForm = ({handleSubmit, onSubmit}) => {
+    return (
+        <form onSubmit={handleSubmit((formValues) => onSubmit(formValues))} className="ui form error">
+            <Field name="title" component={Input} label="Enter Title"/>
+            <Field name="description" component={Input} label="Enter Description"/>
+            <button className="ui button primary">Submit</button>
+        </form>
+    );
+};
 
 const validate = (formValues) => {
     const errors = {};
@@ -52,7 +48,7 @@ const validate = (formValues) => {
     return errors;
 };
 
-const formWrapped =  reduxForm({
+const formWrapped = reduxForm({
     form: 'streamForm',
     validate
 })(StreamForm);
